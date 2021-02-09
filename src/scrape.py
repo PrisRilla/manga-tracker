851e39e1c0ea2b39a80222a8d549d64e9e8f1d05
Priscilla Cheng <priscillacheng@Priscillas-MacBook-Pro.local>
2021-02-08 22:43:13 -0500

    Scrape selected links

--- src/scrape.py
+++ src/scrape.py
@@ -6,13 +6,22 @@ from bs4 import BeautifulSoup
 
 
 def scrape_link(links_to_scrape):
-	print(links_to_scrape[0])
+	"""Scrape based on root"""
+	
+	# Mangatx
 	test_site = links_to_scrape[0]
 	req = Request(test_site, headers={'User-Agent': 'Mozilla/5.0'})
 	web_byte = urlopen(req, timeout=10).read()
 	webpage = web_byte.decode('utf-8')
 	soup = BeautifulSoup(webpage, "html.parser")
-	print(soup)	
+	print(soup.prettify())
+	
+	all_site_links = []
+	for link in soup.find_all('a'):
+		if test_site in link.get('href') and test_site != link.get('href'):
+			if "comment-page" not in link.get('href') and link.get('href') not in all_site_links:
+				all_site_links.append(link.get('href'))
+	print(all_site_links, len(all_site_links))
 
 if __name__=="__main__":
 	with open('/Users/priscillacheng/Documents/projects/manga-tracker/src/data.json') as f:
